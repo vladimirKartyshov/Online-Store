6f0daace9b346f3c687bddb8e32f1dee8e82dceb
vladimirKartyshov <vovan198080@inbox.ru>
2020-09-08 17:40:01 +0300

    Initional project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <Navbar/>
+  </v-app>
+</template>
+
+
+<script>
+  import Navbar from './components/Navbar.vue'
+  export default {
+    components: {
+      Navbar
+    }
+  }
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <v-navigation-drawer app temporary></v-navigation-drawer>
+        <v-app-bar app dark color="primary">
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+            <v-toolbar-title>Online Store</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn flat>
+              Link1
+            </v-btn>
+
+        </v-app-bar>
+
+        <v-main>
+            <router-view></router-view>
+        </v-main>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
