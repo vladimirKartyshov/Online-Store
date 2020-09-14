76d8efdfa1386b53040aa1952f5336b0b1f59cf7
vladimirKartyshov <vovan198080@inbox.ru>
2020-09-14 10:59:12 +0300

    Добавление ссылок navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,38 @@
+
 <template>
     <div>
-        <v-navigation-drawer app temporary></v-navigation-drawer>
+        <v-navigation-drawer app temporary v-model="sideNav">
+            <v-list>
+                <v-list-item
+                    v-for="(link, i) in links"
+                    :key="i"
+                    :to="link.url"
+                    >
+                    <v-list-item-icon>
+                        <v-icon>{{ link.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title v-text="link.title"></v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
         <v-app-bar app dark color="primary">
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-app-bar-nav-icon @click="sideNav = !sideNav"></v-app-bar-nav-icon>
 
             <v-toolbar-title>Online Store</v-toolbar-title>
 
             <v-spacer></v-spacer>
 
-            <v-btn flat>
-              Link1
+            <v-btn
+                    flat
+                    v-for="(link, i) in links"
+                    :key="i"
+                    :to="link.url"
+            >
+                <v-icon left>{{ link.icon }}</v-icon>
+                {{ link.title }}
             </v-btn>
 
         </v-app-bar>
@@ -23,7 +46,18 @@
 
 <script>
     export default {
-        name: "Navbar"
+        data () {
+            return {
+                sideNav: false,
+                links: [
+                    {title: 'Login', icon: 'mdi-account-box', url: '/login'},
+                    {title: 'Register', icon: 'mdi-face', url: '/register'},
+                    {title: 'Cart', icon: 'mdi-cart', url: '/checkout'},
+                    {title: 'New Product', icon: 'mdi-plus', url: '/new'},
+                    {title: 'My Products', icon: 'mdi-format-list-bulleted', url: '/list'},
+                ]
+            }
+        }
     }
 </script>
 
