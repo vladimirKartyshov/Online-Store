061e5c13ce66c393cdde0b2d875a0f375f693f81
vladimirKartyshov <vovan198080@inbox.ru>
2020-09-20 22:08:20 +0300

    Создали роуты по всем страницам

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,55 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../components/Home.vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Product from "@/components/Products/Product";
+import NewProduct from "@/components/Products/NewProduct";
+import ProductList from "@/components/Products/ProductList";
+import Checkout from "@/components/User/Checkout";
+import Login from "@/components/Auth/Login";
+import Register from "@/components/Auth/Register";
 
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+export default new Router({
+  routes: [
+    {
+      path: '',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/product/:id',
+      name: 'product',
+      component: Product
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: ProductList
+    },
+    {
+      path: '/new',
+      name: 'new',
+      component: NewProduct
+    },
+    {
+      path: '/checkout',
+      name: 'checkout',
+      component: Checkout
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+  ],
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL
 })
-
-export default router
