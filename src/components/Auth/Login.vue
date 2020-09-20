f397bbd8ff33824c20858fc987b3b6261984b338
vladimirKartyshov <vovan198080@inbox.ru>
2020-09-20 23:11:54 +0300

    Login page(start)

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,10 +1,25 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Login</h1>
-            </v-flex>
-        </v-layout>
+
+    <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+            <v-col cols="12" sm="8" md="4">
+                <v-card class="elevation-12">
+                    <v-toolbar color="primary" dark flat>
+                        <v-toolbar-title>Login form</v-toolbar-title>
+                    </v-toolbar>
+                    <v-card-text>
+                        <v-form>
+                            <v-text-field label="Email" name="email" prepend-icon="mdi-account" type="email"></v-text-field>
+                            <v-text-field label="Password" name="password" prepend-icon="mdi-lock" type="password"></v-text-field>
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="primary">Login</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
