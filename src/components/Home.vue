3c2e18c57bcf9797c1a1cc39c575c9650e1f185b
vladimirKartyshov <vovan198080@inbox.ru>
2020-10-08 17:38:39 +0300

    Main page corrected

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,12 +17,12 @@
         </v-container>
 
         <v-container>
-            <v-layout row>
+            <v-layout >
                 <v-flex xs12
                         v-for="(item,i) in products"
                         :key="i">
 
-                    <v-card width="400">
+                    <v-card class="mx-auto" width="400">
                         <v-img height="300px"
                                 :src="item.imageSrc">
 
@@ -37,27 +37,15 @@
                             </v-card-title>
                         </v-img>
 
-
-                        <v-card-text>
-                            <div class="font-weight-bold ml-8 mb-2">
-                              
-                            </div>
-
-                            <v-timeline align-top dense>
-                                <v-timeline-item
-                                        v-for="message in messages"
-                                        :key="message.time"
-                                        :color="message.color"
-                                        small>
-                                    <div>
-                                        <div class="font-weight-normal">
-                                            <strong>{{ message.from }}</strong> @{{ message.time }}
-                                        </div>
-                                        <div>{{ message.message }}</div>
-                                    </div>
-                                </v-timeline-item>
-                            </v-timeline>
-                        </v-card-text>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="orange" text>
+                                Share
+                            </v-btn>
+                            <v-btn color="orange" text>
+                                Explore
+                            </v-btn>
+                        </v-card-actions>
                     </v-card>
                 </v-flex>
             </v-layout>
