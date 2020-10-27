91cd18bc22df433741b93cad50902762f2b60061
vladimirKartyshov <vovan198080@inbox.ru>
2020-10-27 21:00:04 +0300

    vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import products from "@/store/modules/products";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  // state: {
+  // },
+  // mutations: {
+  // },
+  // actions: {
+  // },
   modules: {
+    products
   }
 })
